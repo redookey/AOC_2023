7125db42496ca67688856a87c0f7c4db3907cc19
Tomas Michek <tmichek@artex-is.cz>
2023-12-04 17:58:25 +0100

    Day4 progress

--- Day_4/part1.js
+++ Day_4/part1.js
@@ -10,33 +10,22 @@ function main() {
 }
 
 function getTotal(lines) {
+    let totalScore = 0;
     for(const line of lines) {
         const numberGroups = line.slice(line.indexOf(':') + 1).split('|');
         const winningNumbers = numberGroups[0].trim().split(' ').filter((word) => word != '');
         const myNumbers = numberGroups[1].trim().split(' ').filter((word) => word != '');
         let lineTotal = 0;
 
-        for(number in myNumbers) {
-            if (winningNumbers.includes(number)) {
+        for(const number in myNumbers) {
+            if (winningNumbers.includes(myNumbers[number])) {
                 if (lineTotal === 0) { lineTotal = 1; }
                 else { lineTotal *= 2; }
             }
         }
+        totalScore += lineTotal;
     }
+    return totalScore;
 }
 
-function test() {
-    const line = 'Card   1: 27 61 49 69 58 44  2 29 39 10 | 97 96 49 78 26 58 27 77 69  9 39 88 53 10  2 29 61 62 48 87 18 44 74 34 11' 
-    const numberGroups = line.slice(line.indexOf(':') + 1).split('|');
-    console.log(numberGroups);
-    let winningNumbers = numberGroups[0].trim().split(' ').filter((word) => word != '');
-    let myNumbers = numberGroups[1].trim().split(' ');
-    // winningNumbers = winningNumbers.filter((word) => word != '');
-    myNumbers = myNumbers.filter((word) => word != '');
-    console.log(winningNumbers);
-    console.log(myNumbers);
-    
-}
-
-test();
-// main();
+main();
