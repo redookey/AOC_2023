970a2127af7ce0d1e1803dd51ab04b281d9bc0c4
Tomas Michek <tmichek@artex-is.cz>
2023-12-05 01:44:00 +0100

    Day_4 progress

---
+++ Day_4/part2.js
@@ -0,0 +1,42 @@
+const fs = require('fs');
+
+function extractLinesFromInputFile() {
+    const data = fs.readFileSync(__dirname + '/input.txt', 'utf8');
+    return data.split(/\r?\n/);
+}
+
+function main() {
+    console.log(getScratchcardsAmount(extractLinesFromInputFile()));
+}
+
+function getScratchcardsAmount(lines) {
+    let cards = [];
+    let scratchcardsAmount = 0;
+    for(const line of lines) {
+        const numberGroups = line.slice(line.indexOf(':') + 1).split('|');
+        let card = {
+            id: parseInt(line.slice(line.indexOf(' ') + 1, line.indexOf(':'))),
+            winningNumbers: numberGroups[0].trim().split(' ').filter((word) => word != ''),
+            myNumbers: numberGroups[1].trim().split(' ').filter((word) => word != ''),
+            matches: 0,
+            noOfCopies: 0
+        };
+        cards.push(card);
+    }
+
+    for(const card of cards) {
+        for(const number of card.myNumbers) {
+            if (card.winningNumbers.includes(number)) { card.matches++; }
+        }
+
+        for(let i = card.noOfCopies; i >= 0; i--) {
+            for(let i = card.matches; i > 0; i--) {
+                cards.find(c => c.id === card.id + i).noOfCopies++;
+            }
+        }
+        scratchcardsAmount += card.noOfCopies + 1;
+    }
+    return scratchcardsAmount;
+}
+
+main();
