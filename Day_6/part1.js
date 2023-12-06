1a10e1ae912134a8c55d29e819a33686c892cae4
Tomas Michek <tmichek@artex-is.cz>
2023-12-06 12:03:22 +0100

    Day6

---
+++ Day_6/part1.js
@@ -0,0 +1,42 @@
+const fs = require('fs');
+
+function extractLinesFromInputFile() {
+    const data = fs.readFileSync(__dirname + '/input.txt', 'utf8');
+    return data.split(/\r?\n/);
+}
+
+function main() {
+    console.log(solvePuzzle(extractLinesFromInputFile()));
+}
+
+class Race {
+    constructor(availableTime, recordDistance) {
+        this.availableTime = availableTime;
+        this.recordDistance = recordDistance;
+    }
+    numberOfWaysToWin() {
+        let waysToWin = 0;
+        for(let i = 0; i <= this.availableTime; i++) {
+            let speed = i;
+            let remainingTime = this.availableTime - i;
+            if (speed * remainingTime > this.recordDistance) { waysToWin++; }
+        }
+        return waysToWin;
+    }
+    
+}
+
+function solvePuzzle(lines) {
+    const times = lines[0].slice(lines[0].indexOf(':') + 1).trim().split(' ').filter((e) => e);
+    const distances = lines[1].slice(lines[1].indexOf(':') + 1).trim().split(' ').filter((e) => e);
+    let result = 0;
+
+    for(let i = 0; i < times.length; i++) {
+        let waysToWinRace = new Race(times[i], distances[i]).numberOfWaysToWin();
+        if (result) { result *= waysToWinRace; }
+        else { result = waysToWinRace; }
+    }
+    return result;
+}
+
+main();
