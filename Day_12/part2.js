a132dd1438e91cdd494f3a4bc4473345aa7ab72f
redookey <tomas@michek.com>
2023-12-25 18:37:58 +0100

    12 / 2 progress

--- Day_12/part2.js
+++ Day_12/part2.js
@@ -26,18 +26,27 @@ function getConditionRecords(lines) {
     let conditionRecords = [];
     for(const line of lines) {
         let splitLine = line.split(' ');
-        conditionRecords.push(new ConditionRecord(getCopiesWithSeperator(splitLine[0], '?', 4), getCopiesWithSeperator(splitLine[1], ',', 4)));
+        conditionRecords.push(new ConditionRecord(splitLine[0], splitLine[1]));
     }
     return conditionRecords;
 }
 
-function getCopiesWithSeperator(stringToCopy, seperator, numberOfCopies) {
-    let result = stringToCopy;
-    for(let i = 0; i < numberOfCopies; i++) {
-        result = result.concat(seperator, stringToCopy);
-    }
-    return result;
-}
+// function getConditionRecords(lines) {
+//     let conditionRecords = [];
+//     for(const line of lines) {
+//         let splitLine = line.split(' ');
+//         conditionRecords.push(new ConditionRecord(getCopiesWithSeperator(splitLine[0], '?', 4), getCopiesWithSeperator(splitLine[1], ',', 4)));
+//     }
+//     return conditionRecords;
+// }
+
+// function getCopiesWithSeperator(stringToCopy, seperator, numberOfCopies) {
+//     let result = stringToCopy;
+//     for(let i = 0; i < numberOfCopies; i++) {
+//         result = result.concat(seperator, stringToCopy);
+//     }
+//     return result;
+// }
 
 class ConditionRecord {
     constructor(symbolFormat, numberFormat) {
@@ -46,6 +55,7 @@ class ConditionRecord {
         this.numbers = this.getNumbers();
         this.compatibleVariations = this.getCompatibleVariations();
     }
+
     getNumbers() {
         let result = [];
         let numbers = [];
@@ -55,84 +65,60 @@ class ConditionRecord {
         }
         return result;
     }
-    getCompatibleVariations() {
-        const allVariationsSets = this.getAllVariationSets();
-        let compatibleVariations = [];
 
-        for(const variationSet of allVariationsSets) {
-            const symbolRowToValidate = this.getSymbolRowVariation(variationSet);
-            if(this.symbolRowVariationIsValid(symbolRowToValidate)) {
-                if (!compatibleVariations.find(value => value === symbolRowToValidate))  {
-                    compatibleVariations.push(symbolRowToValidate);
-            
-                }
-            }
-        }
-        return compatibleVariations;
-    }
-
-    symbolRowVariationIsValid(symbolRowToValidate) {
-        return (this.numberFormat === getNumbersFormat(symbolRowToValidate));
-    }
-    
-    getSymbolRowVariation(variationSet) {
-        let symbolRowVariation = this.symbolFormat.split('');
-
-        for(let i = 0; i < this.symbolFormat.length; i++) {
-            for(const variation of variationSet) {
-                if(variation[i] === '#') {
-                    symbolRowVariation.splice(i, 1,'#');
-                }
-            }
-        }
-        return symbolRowVariation.join('');
-    }
     getAllVariationSets() {
-        let combinationSets = [];
+        let variations = [];
         let currentNumberIndex = 0;
-        let combinationSet = initArray(this.numbers.length);
+
         this.numbers.sort(bubbleSortByNumberOfCombinations);
+        this.numbers.forEach(value => value.currentCoordinateSetIndex = -1);
+        
+        let baseSymbolRow = this.symbolFormat.split('');
+        let checkpointSymbolRow = baseSymbolRow.map(value => value);
+        
 
-        //[EXPLENATION] set the initial to -1, so the first value isnt skipped
-        //[SYNTAX] also, let and const are influencing wheter the "reassignment" occurs, not the "change of properties" of number, so that's why const is used here
-        this.numbers.forEach(value => value.currentCombinationIndex = -1);
-
-        //TODO replace with an eliminating version
-        //> after each added version to the final combination, check if it is still valid
-        //>> if not, skip the whole sequence and move to the next one
-        //>>> for example: if the 1st and 2nd number do not have a compatible 1st generation combination [1.1 <=> 2.1], move on to the [1.1 <=> 2.1] and skip adding the rest [1.1 <=> 2.1 <=> 3.1]
-        //>>>> simply using  Number.prototype.combinationIsValid() won't work
-        //>>>>> gotta check only the 1st X numbers
-        //>>>>>> scenario: I'm checking 3rd number's 1st generation against the so far made version of the variation [1.1 <=> 2.1 <=> 3.1]
-        //>>>>>>> i create a version of the variation (a part of it) that starts in the very start and ends by the number.zone.endIndex that is being checked, than I send this part to check it against the list of numbers
-        //>>>>>>>> voila, now i can use Number.prototype.combinationIsValid() (just gotta modify it so it acceptss an argument 'numberFormat')
-        /*
         while(currentNumberIndex !== -1) {
             let currentNumber = this.numbers[currentNumberIndex];
+            
             if (currentNumber.succeedingNumbers[0]) {
-                //[EXPLENATION] if there are no more combinations, return one level up + reset currentCombinationIndex
-                if (currentNumber.combinations[currentNumber.currentCombinationIndex + 1]) {
-                    currentNumber.currentCombinationIndex++;
-                    combinationSet.splice(currentNumberIndex, 1, currentNumber.combinations[currentNumber.currentCombinationIndex]);
-                    currentNumberIndex++;
+                if (currentNumber.coordinateSets[currentNumber.currentCoordinateSetIndex + 1]) {
+                    currentNumber.currentCoordinateSetIndex++;
+                    let newlyUpdatedSymbolRow = tryHashtagingAtCoordinates(checkpointSymbolRow, currentNumber.coordinateSets[currentNumber.currentCoordinateSetIndex]);
+                    if (newlyUpdatedSymbolRow) {
+                        checkpointSymbolRow = newlyUpdatedSymbolRow.map(value => value);
+                        currentNumberIndex++;
+                    }
                 }
                 else {
-                    currentNumber.currentCombinationIndex = -1;
                     currentNumberIndex--;
+                    currentNumber.currentCoordinateSetIndex = -1;
                 }
             }
             else {
-                for(const combination of currentNumber.combinations) {
-                    combinationSet.splice(currentNumberIndex, 1, combination);
-                    combinationSets.push([...combinationSet]);
+                for(const coordinateSet of currentNumber.coordinateSets) {
+                    completedSymbolRow = tryHashtagingAtCoordinates(checkpointSymbolRow, coordinateSet);
+                    if (completedSymbolRow) {
+                        variations.push(completedSymbolRow.join(''));
+                    }
                 }
                 currentNumberIndex--;
             }
         }
-*/
-        return combinationSets;
-
+        return variations;
     }
+}
+
+function tryHashtagingAtCoordinates(symbolRow, coordinateSet) {
+    let localSymbolRow = symbolRow.map(value => value);
+    if ((localSymbolRow[coordinateSet.startIndex - 1] !== '#') && (localSymbolRow[coordinateSet.endIndex + 1] !== '#')) {
+        for(let i = coordinateSet.startIndex; i <= coordinateSet.endIndex; i++) {
+            if (localSymbolRow[i] !== '#') {
+                localSymbolRow.splice(i, 1, '#');
+            }
+        }
+        return symbolRow;
+    }
+    return undefined;
 }
 
 class Number {
@@ -142,7 +128,7 @@ class Number {
         this.succeedingNumbers = [...succeedingNumbers];
         this.symbolRow = symbolRow;
         this.zone = this.getZone();
-        this.combinations = this.getCombinations();
+        this.coordinateSets = this.getCoordinateSets();
     }
     getZone() {
         //up my reduce-game? i could define a function that i would pass in as a callback (reduce redunduncy - haha reduce; i know.)
@@ -156,26 +142,31 @@ class Number {
             // numberOfCombinations: wiggleSpace + 1
         };
     }
-    getCombinations() {
+    getCoordinateSets() {
         let result = [];
         for(let i = 0; i < this.zone.numberOfCombinations; i++) {
             let symbolRowVariation = [...this.symbolRow];
             let sequenceStartIndex = this.zone.startIndex + i;
+            let sequenceEndIndex = sequenceStartIndex + this.value - 1;
             const removedSymbols = symbolRowVariation.splice(sequenceStartIndex, this.value, getHashtagString(this.value));
             if (removedSymbols.indexOf('.') !== -1) {
                 continue;
             }
             symbolRowVariation = symbolRowVariation.join('');
-            //TODO validate variation
-           if (this.combinationIsValid(symbolRowVariation, sequenceStartIndex, sequenceStartIndex + this.value - 1)) {
-               result.push(symbolRowVariation);
+           if (coordinateSetIsValid(symbolRowVariation, sequenceStartIndex, sequenceEndIndex)) {
+                let coordinateSet = {
+                    startIndex: sequenceStartIndex,
+                    endIndex: sequenceEndIndex
+                } 
+               result.push(coordinateSet);
            }
         }
         return result;
     }
-    combinationIsValid(symbolRowVariation, sequenceStartIndex, sequenceEndIndex) {
-        return (symbolRowVariation[sequenceStartIndex - 1] !== '#') && (symbolRowVariation[sequenceEndIndex + 1] !== '#');
-    }
+}
+
+function coordinateSetIsValid(symbolRowVariation, sequenceStartIndex, sequenceEndIndex) {
+    return (symbolRowVariation[sequenceStartIndex - 1] !== '#') && (symbolRowVariation[sequenceEndIndex + 1] !== '#');
 }
 
 function bubbleSortByNumberOfCombinations(a, b) {
@@ -188,44 +179,12 @@ function bubbleSortByNumberOfCombinations(a, b) {
     }
 }
 
-function getNumbersFormat(string) {
-    let numbers = [];
-    for(let position = 0; position < string.length; position++) {
-        if (isHashtag(string[position])) {
-            let number = getNumber(string, position);
-            numbers.push(number);
-            position += number;
-        }
-    }
-    return numbers.join(',');
-}
-
-function getNumber(str, position) {
-    let result = 0;
-    for(let i = position; isHashtag(str[i]); i++) {
-        result ++;
-    }
-    return result;
-}
-
-function isHashtag(char) {
-    return (char === '#');
-}
-
 function getHashtagString(length) {
     let result = '';
     for (let i = 0; i < length; i++) {
         result += '#';
     }
     return result;
-}
-
-function initArray(length) {
-    let array = [];
-    for(let i = 0; i < length; i++) {
-        array.push('');
-    }
-    return array;
 }
 
 main();
