2d4ab0524e2e9b0692afb3f2852c891be1a77e9c
redookey <tomas@michek.com>
2024-01-14 19:47:18 +0100

    13 / 1 progress

--- Day_13/part1.js
+++ Day_13/part1.js
@@ -13,7 +13,7 @@
 
 //the confirmation
 //a cycle ->
-//until there is a next number in both directions
+//until there is not a next number in one of the directions
 //get next number on both sides and compare them
 //if they manage to be equal to each other until the end of the cycle, we got it
 
@@ -32,7 +32,14 @@ function main() {
 }
 
 function solvePuzzle(blocks) {
-    
+    let totalLeftSide = 0;
+    let totalAbove = 0;
+    for (const block of blocks) {
+        let blockObject = new Block(block.split(`\n`));
+        totalLeftSide += blockObject.numberOfLeftSideLines;
+        totalAbove += blockObject.numberOfAboveLines;
+    }
+    return (totalLeftSide + (100 * totalAbove));
 }
 
 class Block {
@@ -40,33 +47,45 @@ class Block {
         this.rawLines = rawLines;
         this.horizontalLinesValues = this.getLineValues(this.rawLines);
         this.verticalLinesValues = this.getLineValues(rotateLines(this.rawLines));
+        this.numberOfLeftSideLines = this.scanForMirroring(this.horizontalLinesValues);
+        this.numberOfAboveLines = this.scanForMirroring(this.verticalLinesValues);  //not sure its that easy, is the rotation working correctly for this case?
+
     }
     getLineValues(rawLines) {
         let valueLines = [];
         for(const rawLine of rawLines) {
             let binaryValue = 2;
-             for(const char of rawLine) {
-                let lineValue = 0;
+            for(const char of rawLine) {
+                let lineValue = 0;  //put a level up?
                 if (char === '#') { lineValue += binaryValue; }
                 binaryValue *= 2;
-             }
+            }
             valueLines.push(lineValue);
         }
         return valueLines;
     }
-    // getLineValues(rawLines) {
-    //     let valueLines = [];
-    //     for(const rawLine of rawLines) {
-    //         let coeficient = 2;
-    //          for(const char of rawLine) {
-    //             let valueLine = [];
-    //             char === '#' ? valueLine.push(coeficient) : valueLine.push(0); 
-    //             coeficient *= 2;
-    //          }
-    //         valueLines.push(valueLine);
-    //     }
-    //     return valueLines;
-    // }
+    scanForMirroring(lineValues) {
+        for (let i = 0; i < lineValues.length; i++) {
+            if (lineValues[i] === previousValue) {
+                if (confirmMirroring(i - 0.5)) {
+                    return i; //would be -1, but zero indexing
+                }
+            }
+            let previousValue = value;
+        }
+        return 0;
+    }
+    confirmMirroring(lineValues, mirrorLocation) {
+        let leftSideIndex = Math.floor(mirrorLocation);
+        let rightSideIndex = Math.ceil(mirrorLocation);
+        let i = 0;
+        
+        while(lineValues[leftSideIndex - i] !== -1 && lineValues[rightSideIndex + i] !== -1) {
+            i++;
+            if (lineValues[leftSideIndex - i] !== lineValues[rightSideIndex + i]) { return false; }
+        }
+        return true;
+    }
 }
 
 function rotateLines(lines) {
@@ -82,3 +101,5 @@ function rotateLines(lines) {
     }
     return rotatedLines;
 }
+
+main();
